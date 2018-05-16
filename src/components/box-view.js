4839c663cc2c8c11302e43c5cea39ec161a7679f
Chaur (Gary) Wu <gwu@vmware.com>
2018-05-15 19:38:01 -0700

    Box and Arrow view model

---
+++ src/components/box-view.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+import { observer } from "mobx-react"
+import { DraggableCore } from "react-draggable"
+
+class BoxView extends Component {
+    render() {
+        const { box } = this.props
+        console.log(box);
+        return (
+            <DraggableCore onDrag={this.handleDrag}>
+                <div
+                    style={{
+                        width: box.width,
+                        left: box.x,
+                        top: box.y
+                    }}
+                    onClick={this.handleClick}
+                    className={box.isSelected ? "box box-selected" : "box"}
+                >
+                    {box.name}
+                    {this.props.children}
+                </div>
+            </DraggableCore>
+        )
+    }
+
+    handleClick = e => {
+        this.props.store.setSelection(this.props.box.id)
+        e.stopPropagation()
+    }
+
+    handleDrag = (e, dragInfo) => {
+        this.props.box.move(dragInfo.deltaX, dragInfo.deltaY)
+    }
+}
+
+export default observer(BoxView)
